20c334620df30cbd93acb529cc7a8022904846d7
kshwaikar <148629065+kshwaikar@users.noreply.github.com>
2025-06-21 19:08:43 +0530

    config.py

--- Database/config.py
+++ Database/config.py
@@ -1,7 +1,11 @@
-db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "smartk@24",
-    "database": "life__in_weeks",
-    "connection_timeout": 30
-}
+
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        host="dpg-d1bb6m3e5dus73eef740-a",
+        database="life_in_weeks_db",
+        user="ife_in_weeks_db_user",
+        password=" IhIRHxd4Mz2IQb76ltvIszc4CBpbrk2u",
+        port="5432"  
+    )
