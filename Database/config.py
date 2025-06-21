f9c53956c38ab919976c50e8323adc84f95971eb
kshwaikar <148629065+kshwaikar@users.noreply.github.com>
2025-06-21 19:58:21 +0530

    config.py

--- Database/config.py
+++ Database/config.py
@@ -1,11 +1,10 @@
-
-import psycopg2
+import pg8000.native
 
 def get_connection():
-    return psycopg2.connect(
+    return pg8000.native.Connection(
         host="dpg-d1bb6m3e5dus73eef740-a",
         database="life_in_weeks_db",
-        user="ife_in_weeks_db_user",
-        password=" IhIRHxd4Mz2IQb76ltvIszc4CBpbrk2u",
-        port="5432"  
+        user="life_in_weeks_db_user",  
+        password="IhIRHxd4Mz2IQb76ltvIszc4CBpbrk2u",
+        port=5432  
     )
